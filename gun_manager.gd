a96a99abbe2de09149d5a2dae7af64a116cf26a7
filip.strahov <filip.strahov@seznam.cz>
2025-06-27 02:35:43 +0200

    WeaponManager: simple, not elegant

--- gun_manager.gd
+++ gun_manager.gd
@@ -1,1 +1,60 @@
 extends Node
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("weapon_select"):
+		#print("weapon selector")
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		$AnimationPlayer.play("weapons_appear")
+	elif event.is_action_released("weapon_select"):
+		#print("select gun")
+		weapon_match()
+		
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+		
+
+var weapon
+
+func _on_weapon_1_mouse_entered() -> void:
+	weapon = "Beer"
+	#print("hovered")
+
+
+func _on_weapon_1_mouse_exited() -> void:
+	weapon = -1
+
+func weapon_match():
+	match weapon:
+		"Beer":
+			print("Beer seleced")
+		"Gun":
+			print("Gun selected")
+		"Knife":
+			print("Knife selected")
+		"Grenade":
+			print("Grenade selected")
+		_:
+			print("nothing selected")
+
+
+func _on_weapon_2_mouse_entered() -> void:
+	weapon = "Gun"
+
+
+func _on_weapon_2_mouse_exited() -> void:
+	weapon = -1
+
+
+func _on_weapon_3_mouse_entered() -> void:
+	weapon = "Grenade"
+
+
+func _on_weapon_3_mouse_exited() -> void:
+	weapon = -1
+
+
+func _on_weapon_4_mouse_entered() -> void:
+	weapon = "Knife"
+
+
+func _on_weapon_4_mouse_exited() -> void:
+	weapon = -1
